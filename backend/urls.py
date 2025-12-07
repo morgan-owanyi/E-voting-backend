151b6ccf0f3390b4335760ff55990c6d5854f8a4
morgan owanyi <morganowanyii>
2025-12-07 10:27:54 +0300

    modified the backend/urls.py and accounts/views.py to fix the LoginView class definition and added the election app to the project settings and urls.

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/accounts/", include("accounts.urls")),
-     path("api/election/", include("election.urls")),
+    path("api/accounts/", include("accounts.urls")),#accounts endpoint
+     path("api/election/", include("election.urls")),#election endpoint
 ]
 
